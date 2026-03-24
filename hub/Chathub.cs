bd895692b02a999874aecaa3f2e22b167e1c64e1
salmamagdi <salmamagdi637@yahoo.com>
2026-03-24 08:28:19 +0200

    add backend

---
+++ hub/Chathub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace chatapp.hub
+{
+    public class Chathub : Hub
+    {
+
+        public async Task<string> SendAsync(string user,string message)
+        {
+           await Clients.All.SendAsync("receive message",user,message);
+
+            return null;
+        }
+    }
+}
