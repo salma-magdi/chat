b5666155e9651041cb79c14ba6560f490e108db8
salmamagdi <salmamagdi637@yahoo.com>
2026-03-24 07:43:28 +0200

    upload full Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { SignalRComponent } from './signal-r/signal-r.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,FormsModule,SignalRComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
